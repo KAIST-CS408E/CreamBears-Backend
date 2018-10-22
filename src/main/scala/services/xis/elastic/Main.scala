bc31c8b3a7eb7fc03478f633d882e358020bd887
Jaemin Hong <hjm0901@gmail.com>
2018-10-22 05:29:47 +0000

    import crawler library

--- src/main/scala/services/xis/elastic/Main.scala
+++ src/main/scala/services/xis/elastic/Main.scala
@@ -1,7 +1,15 @@
 package services.xis.elastic
 
+import scala.collection.mutable.{Map => MMap}
+
+import services.xis.crawl.ConnectUtil._
+import services.xis.crawl.LoginUtil._
+import services.xis.crawl.CrawlUtil._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    implicit val cookies: Cookie = MMap()
+    login
+    println(getMax("today_notice"))
   }
 }
