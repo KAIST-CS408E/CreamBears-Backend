26de8f7722c0dd0807546125f945712e49c03fb6
Jaemin Hong <hjm0901@gmail.com>
2018-10-27 09:41:55 +0000

    add tests for ArticleGetter

--- src/test/scala/services/xis/elastic/IndexerSpec.scala
+++ src/test/scala/services/xis/elastic/IndexerSpec.scala
@@ -16,6 +16,13 @@ class IndexerSpec extends TestKit(ActorSystem("IndexerSpec"))
 
   private implicit val cookie: Cookie = MMap()
 
+  private val requestId = 42
+  private val max = 2329
+  private val len = 15
+  private val board = "student_notice"
+  private val id = "11540544028732"
+  private val title = "제4회 융합기초학부 이해를 위한 공개세미나 참석 신청 안내"
+
   override def beforeAll: Unit = {
     login
   }
@@ -28,20 +35,32 @@ class IndexerSpec extends TestKit(ActorSystem("IndexerSpec"))
     val probe = TestProbe()
     val maxGetter = system.actorOf(MaxGetter.props)
 
-    maxGetter.tell(MaxGetter.Request(42), probe.ref)
+    maxGetter.tell(MaxGetter.Request(requestId), probe.ref)
     val response = probe.expectMsgType[MaxGetter.Success]
-    response.requestId should ===(42)
-    response.max should be >= 2329
+    response.requestId should ===(requestId)
+    response.max should be >= max
   }
 
   "IndexGetter should work" in {
     val probe = TestProbe()
     val maxGetter = system.actorOf(SummaryGetter.props)
 
-    maxGetter.tell(SummaryGetter.Request(42, 1), probe.ref)
+    maxGetter.tell(SummaryGetter.Request(requestId, 1), probe.ref)
     val response = probe.expectMsgType[SummaryGetter.Result]
-    response.requestId should ===(42)
+    response.requestId should ===(requestId)
     response.page should ===(1)
-    response.summaries should have length 15
+    response.summaries should have length len
+  }
+
+  "ArticleGetter should work" in {
+    val probe = TestProbe()
+    val maxGetter = system.actorOf(ArticleGetter.props)
+
+    maxGetter.tell(ArticleGetter.Request(requestId, board, id), probe.ref)
+    val response = probe.expectMsgType[ArticleGetter.Success]
+    response.requestId should ===(requestId)
+    response.board should ===(board)
+    response.id should ===(id)
+    response.article.title should ===(title)
   }
 }
