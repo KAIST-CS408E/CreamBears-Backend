a730d0093380ffeee9e0ca36592aa17c530729e5
Jaemin Hong <hjm0901@gmail.com>
2018-10-28 13:27:27 +0000

    better command line options

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # xIS-Backend
+* Crawl articles from KAIST Portal.
+* Extract texts from images and attached files in the articles.
+* Index articles into Elasticsearch.
+## How to use
+* Production mode
+```shell
+$ sbt
+sbt:xis-elastic> run [index] [type]
+```
+* Debug mode
+```shell
+$ sbt
+sbt:xis-elastic> run [index] [type] --debug [start] [end]
+```
+* Type `exit` to finish
+* Press the enter key to see runtime information
--- src/main/scala/services/xis/elastic/Main.scala
+++ src/main/scala/services/xis/elastic/Main.scala
@@ -13,43 +13,42 @@ object Main {
   private val port0 = 9200
   private val port1 = 9300
   private val protocol = "http"
-  private val index = "portal2"
-  private val typ = "article"
 
-  def main(args: Array[String]): Unit = {
-    val indexer = new Indexer(host, port0, port1, protocol, index, typ)
+  def main(args: Array[String]): Unit = (args.toList match {
+    case index :: typ :: tail =>
+      val indexer = new Indexer(host, port0, port1, protocol, index, typ)
+      val system = ActorSystem("index-system")
+      val manager = tail match {
+        case "--debug" :: start :: end :: Nil =>
+          system.actorOf(
+            CrawlManager.debugProps(
+              start = start.toInt,
+              end = end.toInt,
+              indexer = indexer,
+              maxWorkerNum = 1,
+              summaryWorkerNum = 3,
+              articleWorkerNum = 20,
+              fileWorkerNum = 5,
+              extractWorkerNum = 50,
+              readWorkerNum = 1,
+              writeWorkerNum = 10))
+        case _ =>
+          system.actorOf(
+            CrawlManager.props(
+              indexer = indexer,
+              maxWorkerNum = 1,
+              summaryWorkerNum = 3,
+              articleWorkerNum = 20,
+              fileWorkerNum = 5,
+              extractWorkerNum = 50,
+              readWorkerNum = 1,
+              writeWorkerNum = 10))
+      }
+      Some(system, indexer, manager)
+    case _ => None
+  }).map{ case (system, indexer, manager) =>
     indexer.createIndex()
-
-    val system = ActorSystem("index-system")
-
-    val manager = args.toList match {
-      case "--debug" :: start :: end :: Nil =>
-        system.actorOf(
-          CrawlManager.debugProps(
-            start = start.toInt,
-            end = end.toInt,
-            indexer = indexer,
-            maxWorkerNum = 1,
-            summaryWorkerNum = 3,
-            articleWorkerNum = 20,
-            fileWorkerNum = 5,
-            extractWorkerNum = 50,
-            readWorkerNum = 1,
-            writeWorkerNum = 10))
-      case _ =>
-        system.actorOf(
-          CrawlManager.props(
-            indexer = indexer,
-            maxWorkerNum = 1,
-            summaryWorkerNum = 3,
-            articleWorkerNum = 20,
-            fileWorkerNum = 5,
-            extractWorkerNum = 50,
-            readWorkerNum = 1,
-            writeWorkerNum = 10))
-    }
     manager ! CrawlManager.Start
-
     try {
       while (StdIn.readLine().toLowerCase != "exit")
         manager ! CrawlManager.Info
